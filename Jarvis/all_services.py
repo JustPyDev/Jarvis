e980fcf55b6ca6d363c05768c261cdcf632c9440
Pydeveloped <94559562+Pydeveloped@users.noreply.github.com>
2022-05-22 21:16:01 +0300

    Jarvis
    Jarvis

---
+++ Jarvis/all_services.py
@@ -0,0 +1,11 @@
+# barcha so`zlar jamlanmasi
+opts = {
+    "alias": ('jarvis', 'jarvis', 'jar', 'jorvis', 'jarviz', 'jorviz',
+              'yarvis', '', 'jorvez', 'jarvez', 'jarves'),
+    "tbr": ('aytginchi', 'ko`rsatchi', 'korsat', 'necha', 'aytchi'),
+    "cmds": {
+        "ctime": ('xozirgi vaqt', 'soat necha', 'xozirgi soat'),
+        "radio": ('muzikani qo`y', 'radioni yoq', 'radiyoni qo`sh'),
+        "stupid1": ('latifa aytib ber', 'meni kuldir', 'Latifa aytishni bilasanmi')
+    }
+}
